9e7c6442c24bfcbcf110ceb09588440f84070808
shaimeur <haimeursaad@gmail.com>
2023-05-28 20:22:30 +0100

    fixing the post request problem now I need to deal with the migrations

--- ETLAPI/models.py
+++ ETLAPI/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from sqlalchemy import Column, Integer, String, Date, Float
+from ETLAPI.database import Base
 
-# Create your models here.
+class Transaction(Base):
+    __tablename__ = 'transactions'
+
+    id = Column(Integer, primary_key=True)
+    transaction_id = Column(Integer)
+    transaction_date = Column(Date)
+    amount = Column(Float)
+    merchant_name = Column(String(100))
--- ETLAPI/views.py
+++ ETLAPI/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import pandas as pd
+from io import StringIO
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from ETLAPI.models import Transaction
+from ETLAPI.database import db_session
 
-# Create your views here.
+@csrf_exempt
+def import_csv(request):
+    if request.method == 'POST':
+        csv_file = request.FILES.get('file')
+        if csv_file is None:
+            return HttpResponse("No CSV file provided.")
+
+        csv_data = csv_file.read().decode('utf-8')
+        csv_df = pd.read_csv(StringIO(csv_data))
+
+        for _, row in csv_df.iterrows():
+            transaction = Transaction(
+                transaction_id=row['Transaction ID'],
+                transaction_date=row['Transaction Date'],
+                amount=row['Amount'],
+                merchant_name=row['Merchant Name']
+            )
+            db_session.add(transaction)
+
+        db_session.commit()
+
+        return HttpResponse("CSV data imported successfully!")
+
+    return HttpResponse("Invalid request method.")
